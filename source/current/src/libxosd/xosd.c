8b0d7cdc65817fa349a6c3cbc05fabba1fb23d27
pmhahn <pmhahn@3a7ed7e4-6e13-0410-ba71-8ed462f9d0c2>
2006-05-10 13:22:23 +0000

    Move includes and definitions to internal header
    git-svn-id: svn://svn.code.sf.net/p/libxosd/code@457 3a7ed7e4-6e13-0410-ba71-8ed462f9d0c2

--- source/current/src/libxosd/xosd.c
+++ source/current/src/libxosd/xosd.c
@@ -17,61 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc., 
  * 675 Mass Ave, Cambridge, MA 02139, USA. 
  */
-
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <time.h>
-#include <sys/select.h>
-
-#include <assert.h>
-#include <pthread.h>
-#include <errno.h>
-
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/extensions/shape.h>
-#include <X11/Xatom.h>
-#ifdef HAVE_XINERAMA
-#  include <X11/extensions/Xinerama.h>
-#endif
-
-#include "xosd.h"
-
-/* gcc -O2 optimizes debugging away if Dnone is chosen. {{{ */
-static const enum DEBUG_LEVEL {
-  Dnone = 0,          /* Nothing */
-  Dfunction = (1<<0), /* Function enter/exit */
-  Dlocking = (1<<1),  /* Locking */
-  Dselect = (1<<2),   /* select() processing */
-  Dtrace = (1<<3),    /* Programm progess */
-  Dvalue = (1<<4),    /* Computed values */
-  Dupdate = (1<<5),   /* Display update processing */
-  Dall = -1           /* Everything */
-} _xosd_debug_level = Dnone;
-#define DEBUG(lvl, fmt, ...) \
-  do { \
-    if (_xosd_debug_level & lvl) \
-      fprintf (stderr, "%s:%-4d %ld@%s: " fmt "\n", __FILE__, __LINE__, \
-          pthread_self(), __PRETTY_FUNCTION__ ,## __VA_ARGS__); \
-  } while (0)
-#define FUNCTION_START(lvl) \
-  do { \
-    if (_xosd_debug_level & Dfunction && _xosd_debug_level & lvl) \
-      fprintf (stderr, "%s:%-4d %ld<%s\n", __FILE__, __LINE__, \
-          pthread_self(), __PRETTY_FUNCTION__); \
-  } while (0)
-#define FUNCTION_END(lvl) \
-  do { \
-    if (_xosd_debug_level & Dfunction && _xosd_debug_level & lvl) \
-      fprintf (stderr, "%s:%-4d %ld>%s\n",  __FILE__, __LINE__, \
-          pthread_self(), __PRETTY_FUNCTION__); \
-  } while (0)
-/* }}} */
+#include "intern.h"
 
 #define SLIDER_SCALE 0.8
 #define SLIDER_SCALE_ON 0.7
@@ -83,83 +29,6 @@ const char *osd_default_font =
   "-adobe-helvetica-bold-r-*-*-10-*";
 #endif
 const char *osd_default_colour = "green";
-
-typedef struct
-{
-  enum { LINE_blank, LINE_text, LINE_percentage, LINE_slider } type;
-
-  char *text;
-  int width;
-  int percentage;
-} xosd_line;
-
-struct xosd
-{
-  pthread_t event_thread;       /* CONST handles X events */
-
-  pthread_mutex_t mutex;        /* CONST serialize X11 and structure */
-  pthread_cond_t cond_wait;     /* CONST signal X11 done */
-  int pipefd[2];                /* CONST signal X11 needed */
-
-  pthread_mutex_t mutex_hide;   /* CONST mutual exclusion for show/hide */
-  pthread_cond_t cond_hide;     /* CONST signal hide events */
-
-  Display *display;             /* CONST x11 */
-  int screen;                   /* CONST x11 */
-  Window window;                /* CONST x11 */
-  unsigned int depth;           /* CONST x11 */
-  Pixmap mask_bitmap;           /* CACHE (font,offset) XShape mask */
-  Pixmap line_bitmap;           /* CACHE (font,offset) offscreen bitmap */
-  Visual *visual;               /* CONST x11 */
-
-  XFontSet fontset;             /* CACHE (font) */
-  XRectangle *extent;           /* CACHE (font) */
-
-  GC gc;                        /* CONST x11 */
-  GC mask_gc;                   /* CONST x11 white on black to set XShape mask */
-  GC mask_gc_back;              /* CONST x11 black on white to clear XShape mask */
-
-  int screen_width;             /* CONST x11 */
-  int screen_height;            /* CONST x11 */
-  int screen_xpos;              /* CONST x11 */
-  int height;                   /* CACHE (font) */
-  int line_height;              /* CACHE (font) */
-  xosd_pos pos;                 /* CONF */
-  xosd_align align;             /* CONF */
-  int hoffset;                  /* CONF */
-  int voffset;                  /* CONF */
-  int shadow_offset;            /* CONF */
-  XColor shadow_colour;         /* CONF */
-  unsigned long shadow_pixel;   /* CACHE (shadow_colour) */
-  int outline_offset;           /* CONF */
-  XColor outline_colour;        /* CONF */
-  unsigned long outline_pixel;  /* CACHE (outline_colour) */
-  int bar_length;               /* CONF */
-
-  int mapped;                   /* DYN */
-  int done;                     /* DYN */
-  enum {
-    UPD_none = 0,       /* Nothing changed */
-    UPD_hide = (1<<0),  /* Force hiding */
-    UPD_show = (1<<1),  /* Force display */
-    UPD_timer = (1<<2), /* Start timer */
-    UPD_pos = (1<<3),   /* Reposition window */
-    UPD_lines = (1<<4), /* Redraw content */
-    UPD_mask = (1<<5),  /* Update mask */
-    UPD_size = (1<<6),  /* Change font and window size */
-    UPD_content = UPD_mask | UPD_lines,
-    UPD_font = UPD_size | UPD_mask | UPD_lines | UPD_pos
-  } update;                     /* DYN */
-
-  unsigned long pixel;          /* CACHE (pixel) */
-  XColor colour;                /* CONF */
-
-  xosd_line *lines;             /* CONF */
-  int number_lines;             /* CONF */
-
-  int timeout;                  /* CONF delta time */
-  struct timeval timeout_start; /* DYN Absolute start of timeout */
-};
 
 /** Global error string. */
 char *xosd_error;
