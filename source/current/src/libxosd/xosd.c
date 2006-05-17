24eaf4bd98393f76dab2fb82b430ad17c5c8a09a
pmhahn <pmhahn@3a7ed7e4-6e13-0410-ba71-8ed462f9d0c2>
2006-05-17 12:07:34 +0000

    Update COPYING: the FSF has relocated Licence change from GPL to LGPL for libxosd only
    git-svn-id: svn://svn.code.sf.net/p/libxosd/code@609 3a7ed7e4-6e13-0410-ba71-8ed462f9d0c2

--- source/current/src/libxosd/xosd.c
+++ source/current/src/libxosd/xosd.c
@@ -1,21 +1,23 @@
 /*
- * XOSD
- * 
- * Copyright (c) 2000 Andre Renaud (andre@ignavus.net)
- * 
- * This program is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * XOSD - X On-Screen Display library
+ *
+ * Copyright (C) 2000-2002 Andre Renaud <andre@ignavus.net>
+ * Copyright (C) 2002-2005 Tim Wright <tim@ignavus.net>
+ * Copyright (C) 2005-2006 Philipp Hahn <pmhahn@debian.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 675 Mass Ave, Cambridge, MA 02139, USA. 
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #include "intern.h"
 
